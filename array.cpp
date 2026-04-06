c81b0b794279b0a30b251117bec46701b76ef95a
aliyanadaa <aliyanadaq@gmail.com>
2026-04-06 15:27:42 +0700

    mengisi satu persatu

--- array.cpp
+++ array.cpp
@@ -8,5 +8,16 @@ int main (){
     aku [1] = 200;
     cout << endl;
     cout << "Data pada index 1 = " << aku[1] << endl;
+    cout << "Data pada index 0 =  ";
+    cin >> kamu [0];
+    cout << "Data pada index 1 =  ";
+    cin >> kamu [1];
+    cout << "Data pada index 2 =  ";
+    cin >> kamu [2];
+    cout << "Data pada index 3 =  ";
+    cin >> kamu [3];
+    cout << "Data pada index 4 =  ";
+    cin >> kamu [4];
+    cout << endl;
 
 }
